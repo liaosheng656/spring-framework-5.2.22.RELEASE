df59bd9dab0780bae0f2a3f0f258077c622b34fe
liaosheng656 <1912973090@qq.com>
2023-02-18 10:33:40 +0800

    本地修改

--- Spring-Demo1/src/main/java/org/af/RunSpringDemo.java
+++ Spring-Demo1/src/main/java/org/af/RunSpringDemo.java
@@ -1,6 +1,7 @@
 package org.af;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 启动类
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @date: 2023/02/17
  *
  */
+@Configuration
 public class RunSpringDemo {
 
 	public static void main(String[] args) {
@@ -15,6 +17,8 @@ public class RunSpringDemo {
 		//创建容器对象
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RunSpringDemo.class);
 //		context.getBean()
+
+		System.out.println("执行完了-----");
 
 	}
 
